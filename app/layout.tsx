cce67c164e6e97ba991bd87339f1f3d8d1d3040b
SyedMash <mashrtx7@gmail.com>
2025-07-15 07:22:14 -0700

    feat: integrate Facebook Pixel tracking in layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import ScrollTop from "@/components/ScrollTop";
 import { Toaster } from "@/components/ui/sonner";
 import Preloader from "@/components/PreLoader";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -69,6 +70,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '772180308506240');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        {/* ✅ Noscript fallback */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=772180308506240&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </head>
       <body
         className={`${inter.className} ${manrope.variable} ${firaSans.variable} bg-brand-black text-brand-offwhite antialiased`}
       >
